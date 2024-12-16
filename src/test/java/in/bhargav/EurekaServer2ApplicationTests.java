cdec7a190f44b07e3deb067cdcaa388a235fdf0b
puligallabhargav05 <bhargavpuligalla3@gmail.com>
2024-12-16 17:39:29 +0530

    first commit

---
+++ src/test/java/in/bhargav/EurekaServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bhargav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
